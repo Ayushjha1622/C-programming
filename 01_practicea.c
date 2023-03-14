4cc370863c4ee3272a6f7b025018e1b4a370d13c
Ayush jha <119880444+Ayushjha1622@users.noreply.github.com>
2023-03-14 15:19:08 +0530

    Add files via upload

---
+++ 01_practicea.c
@@ -0,0 +1,20 @@
+// #include<stdio.h>
+// int main() {
+//     int length = 3,breadth = 5;
+//     int area = length*breadth;
+//     printf("the area of this rectangle is %d", area);
+//     return 0;
+// }
+
+
+
+
+
+#include<stdio.h>
+int main() {
+    int length=4;
+    int breadth=5;
+    int area = length*breadth;
+    printf("the area of rectangle is %d", area); 
+    return 0;
+}
