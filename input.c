0443f5306856ef8975e213a794fa36cc6b11de07
Ayush jha <119880444+Ayushjha1622@users.noreply.github.com>
2023-03-15 17:46:12 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,34 @@
+// # include <stdio.h>
+
+// int main() 
+// {
+//     int a, b;
+//     printf("enter the value of a\n");
+//     scanf("%d", &a);
+
+//     printf("enter the value of b\n");
+//     scanf("%d", &b);
+
+//     printf("the sum of a and b is %d", a + b);
+//     return 0;
+// }
+
+
+
+
+
+#include<stdio.h>
+int main() {
+    int a,b;
+
+    printf("the value of a is: \n");
+    scanf("%d", &a);
+
+    printf("the value of b is: \n");
+    scanf("%d", &b);
+
+    printf("the sum of a and b is: %d", a+b);
+    return 0;
+}
+
+
